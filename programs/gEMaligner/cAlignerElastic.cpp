636aebcd5f91b291e713475edddca004039fa46b
Thai Hoang <hvthaibk@gmail.com>
2016-04-16 01:25:01 +0200

    add testing data for gEMpicker and gEMfitter

--- programs/gEMaligner/cAlignerElastic.cpp
+++ programs/gEMaligner/cAlignerElastic.cpp
@@ -15,7 +15,7 @@
 void cAligner::alignElastic(void)
 {
     // output directory
-    dirDataTmp1 = dirDataOut + "/align";
+    dirDataTmp1 = dirDataOut + "/rigid";
     dirDataTmp2 = dirDataOut + "/tps";
 
     cFileSystem     objFileSystem(dirDataTmp2);
@@ -30,27 +30,18 @@ void cAligner::alignElastic(void)
 
     startOpenMP();
 
-// tracking time: 2797 (s)
-// tracking time without correlation: 19 (s)
-// matrix time Armadillo: 755 (s)
-// matrix time Eigen:     227 (s)
-// matrix time without inversion: 14 (s)
-// warping time GPU: 1913 (s)
-// warping time 32 CPU: 31121
-
     // ---------------------
     // Point tracking
     // ---------------------
-/*
     cSize3                  tplSizeHalf(10,10,0),
                             searchRange(10,10,0),
                             step(10,10,0),
                             border = tplSizeHalf + searchRange + 1;
-*/
-    cSize3                  tplSizeHalf(50,50,0),
+
+    /*cSize3                  tplSizeHalf(50,50,0),
                             searchRange(30,30,0),
                             step(50,50,0),
-                            border = tplSizeHalf + searchRange + 1;
+                            border = tplSizeHalf + searchRange + 1;*/
 
     cStackRegTPS2<float>    objRegTPS2;
 
